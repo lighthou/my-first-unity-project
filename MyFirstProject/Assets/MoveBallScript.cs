7ffca77fd28425b763644d2377ff97a66b058001
Kurz <ryan.kurz@sap.com>
2018-08-30 09:52:45 +1000

    simple moving ball with arrow keys

---
+++ MyFirstProject/Assets/MoveBallScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 vel = rb.velocity;
+            vel.y += 2;
+            rb.velocity = vel;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 vel = rb.velocity;
+            vel.y -= 2;
+            rb.velocity = vel;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 vel = rb.velocity;
+            vel.x += 2;
+            rb.velocity = vel;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 vel = rb.velocity;
+            vel.x -= 2;
+            rb.velocity = vel;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
